30e28b3eef22a3399f9caeeb91f39ae1252393e8
Sanjith Udupa <sanjith.udupa@gmail.com>
2020-11-13 17:54:36 -0500

    removed compiled ts files

--- out/index.js
+++
@@ -1,6 +0,0 @@
-import { Block, Text } from './block.js';
-import { render } from './renderer.js';
-let newBlock = new Block(() => {
-    return new Text("hello there what is up");
-});
-render(newBlock);
--- out/renderer.js
+++
@@ -1,4 +0,0 @@
-export function render(block) {
-    let body = block.body();
-    document.querySelector("body").innerHTML += body.html;
-}
