22eaf4970496e6c5cf55736f25b0ec3f75d24c72
Sanjith Udupa <sanjith.udupa@gmail.com>
2020-11-17 16:58:53 -0500

    cleaned up renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,19 +1,11 @@
 import {Block} from "./block.js";
 
-let viewBlock;
-
 export function render(block : Block){
     let body:HTMLElement = block.body().get();
-    viewBlock = block;
-
-    let already = document.getElementById("stacks_js_view");
-    if(already) {
-        already.remove()
-    }
 
     document.body.appendChild(body);
     
-    window.onresize = (e) => {
+    window.onresize = () => {
         body.style.height = window.innerHeight.toString() + "px";
     }
 }
