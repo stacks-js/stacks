1db299bca8016e262d24e42fd26b9c774ec71def
Sanjith Udupa <sanjith.udupa@gmail.com>
2020-11-16 20:38:35 -0500

    converted from strings to more of an element format to greatly simplify the logic of creating new blocks

--- src/block.ts
+++ src/block.ts
@@ -3,20 +3,31 @@ export class Block {
     html: string;
     isLink: boolean = false;
 
-    params = {};
+    params = {
+        tag: "div",
+        isLink: false
+    };
 
     link(href:string){
         this.isLink = true;
         this.params["href"] = href;
+        this.params["tag"] = "a";
         return this;
     }
 
     get() {
-        let mainTag = this.isLink ? "a" : "div";
-        let front = "<" + mainTag + (this.isLink ? " href='" + this.params["href"] + "'>" : ">");
-        let back = "</" + mainTag + ">";
+        // let mainTag = this.isLink ? "a" : "div";
+        // let front = "<" + mainTag + (this.isLink ? " href='" + this.params["href"] + "'>" : ">");
+        // let back = "</" + mainTag + ">";
 
-        let fullhtml = front + this.html + back;
-        return fullhtml;
+        let element = document.createElement(this.params["tag"])
+        element.id = this.constructor.name;
+        element.innerHTML = this.html;
+
+        if(this.isLink) {
+            element.setAttribute("href", this.params["href"]);
+        }
+
+        return element;
     }
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -2,6 +2,6 @@ import {Block} from "./block.js";
 
 export function render(block : Block){
     let body = block.body();
-    document.querySelector("body").innerHTML += body.get();
+    document.querySelector("body").appendChild(body.get())
     console.log(body.get())
 }
