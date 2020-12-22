4925e52da4588c6b9660a8ac9fd60987e3b87b96
Sanjith Udupa <sanjith.udupa@gmail.com>
2020-12-22 12:25:48 -0500

    Started working on setting up the npm project so I can push to production eventually

---
+++ src/stacks.ts
@@ -0,0 +1,34 @@
+import { Block } from "./block.js";
+import { Text } from "./text.js";
+import { Image } from "./image.js";
+import { Stack } from "./stack.js";
+import { StacksRenderer } from "./renderer.js";
+import { Margin } from "./margin.js";
+
+const Utils = {
+    Margin
+}
+
+// const TText = function(txt, style) {
+//     return new text(txt, style);
+// }
+
+// const Text = text;
+
+// const Image = function(path) {
+//     return new image(path);
+// }
+
+// const Stack = function(type, ...blocks) {
+//     return new stack(type, ...blocks);
+// }
+
+export {
+    Block,
+    Text,
+    // TText,
+    Image,
+    Stack,
+    StacksRenderer,
+    Utils
+}
--- stacks.js
+++
@@ -1,6 +0,0 @@
-export { Block } from "./dist/block.js";
-export { Text } from "./dist/text.js";
-export { Image } from "./dist/image.js";
-export { Stack } from "./dist/stack.js";
-export { StacksRenderer } from "./dist/renderer.js";
-export { Margin } from "./dist/margin.js";
